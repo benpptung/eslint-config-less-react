e8d4798cfcf71afe589b5e9311ae83d891e4ae98
benpptung <benpptung@tacol.biz>
2017-08-28 13:30:44 +0800

    update rules - react plugin cannot detect traditional prototype React component

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ module.exports = {
       "allowBind": false
     }],
     "react/jsx-no-target-blank":[1],
-    "react/jsx-tag-spacing":[1]
+    "react/jsx-tag-spacing":[1],
+    "react/prop-types": [0]
   }
 };
